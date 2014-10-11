d62565dde5d85ad1dcd3269325769d6898c1c18c
Alexey <payalnik@gmail.com>
2014-10-11 11:46:04 -0700

    Emptied config file

--- config.py
+++ config.py
@@ -5,6 +5,9 @@ sqlbackupstr = "mysqldump --all-databases -uUSERNAME -pPASS -r backup/backup.sql
 backup_folder_name = 'backup_'
 delete_file = False
 archpass = ''
+mail_from = ''
+mail_to = ''
+
 
 sync_dirs = [
 "/var/www",
